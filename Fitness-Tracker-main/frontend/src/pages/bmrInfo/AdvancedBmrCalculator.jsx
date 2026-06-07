a033874de132feaa73e9c8d07a94b1ab44d6125e
Pranesh K <kpranesh2004@gmail.com>
2026-06-07 16:21:00 +0530

    Update AdvancedBmrCalculator.jsx

--- Fitness-Tracker-main/frontend/src/pages/bmrInfo/AdvancedBmrCalculator.jsx
+++ Fitness-Tracker-main/frontend/src/pages/bmrInfo/AdvancedBmrCalculator.jsx
@@ -1,4 +1,4 @@
-/* Advanced 3D BMR Calculator with Nutrition Goal */
+/* Advanced 3D BMR Calculator with Nutrition Goals */
 import React, { useState, useContext, useRef } from 'react';
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
